259cb8032743b7b23f9d254977cdbdc1ae969c2e
Devil7DK <dineshthangavel47@gmail.com>
2021-10-22 18:20:21 +0530

    Fix typo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 const bundler = new Parcel({
     defaultConfig: "@parcel/config-default",
-    entrypoints: [join(process.cwd(), 'index.html')]
+    entries: [join(process.cwd(), 'index.html')]
 });
 
 bundler.run().then(console.log).catch(console.error);
